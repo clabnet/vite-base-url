73977e7e8b72d6baa0a3573edb740cc79a12b2cf
Claudio Barca <claudio.barca@teoresigroup.com>
2022-06-19 07:26:55 +0200

    add server and preview to vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { fileURLToPath } from 'url'
 import { defineConfig, loadEnv } from 'vite'
 import type { ConfigEnv } from 'vite'
@@ -10,15 +11,34 @@ export default (configEnv: ConfigEnv) => {
   return defineConfig({
     base: process.env.VITE_APP_BASE_URL,
 
+    root: './',
+
+    server: {
+      host: '0.0.0.0',
+      port: Number(process.env.VITE_SERVER_PORT),
+      strictPort: true
+      //   cors: true,
+      //  origin: 'http://localhost:3000',
+    },
+
+    preview: {
+      host: '0.0.0.0',
+      port: Number(process.env.VITE_SERVER_PORT),
+      strictPort: true
+    },
+
     plugins: [vue()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
+
     build: {
       // StackBlitz does not support Brotli
       brotliSize: false,
+      // outDir: path.join(__dirname, "dist"),
     },
+
   })
 }
